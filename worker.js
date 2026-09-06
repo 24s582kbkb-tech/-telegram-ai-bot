d1fea5097b482d30c995caf54b5abc5b57ac08dd
24s582kbkb-tech <24s582kbkb@privaterelay.appleid.com>
2026-09-06 17:37:28 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 const BOT_TOKEN = "8233740982:AAHQyOqaXirTJrO8plOH0QPgMpvuDG2oBSQ";
-const HF_TOKEN = "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";
+const HF_TOKEN = "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";
 
 export default {
   async fetch(request, env) {
